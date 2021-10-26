efdb0f2b58bce79faab9c587bf920e4f240fa1af
Eray C <hi@eri.gg>
2021-10-26 11:37:04 +0200

    dep: Ajout de couleur jaune pour background

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      yellowbright: "FFF6D6",
+    },
     extend: {},
   },
   variants: {
