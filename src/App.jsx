26ce5f64e8e960fe1cd61869793e9a22164aa814
Eray C <hi@eri.gg>
2021-11-01 19:24:55 +0100

    ref: Move all child components to parent (App)

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,19 @@ import "./index.css";
 // Components
 // import { Loading } from "./components/Loading";
 import { Navbar } from "./components/Navbar";
+import { IngredientBox } from "./components/IngredientBox";
+import { Button } from "./components/Button";
 
-// Views
-import { ChooseBread } from "./views/ChooseBread";
-import { ChooseMeat } from "./views/ChooseMeat";
-import { ChooseVegetables } from "./views/ChooseVegetables";
-import { ChooseSauces } from "./views/ChooseSauces";
-
-import { defaultOrder } from "./data";
+import { defaultOrder, defaultBreads, defaultMeats, defaultVegetables, defaultSauces } from "./data";
 
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {
-      currentOrder: defaultOrder,
-      isLoading: true,
-      timerHandle: null,
+    this.state = { currentOrder: defaultOrder, isLoading: true, timerHandle: null,
     };
   }
 
-  componentDidMount() {
-    this.timerHandle = setTimeout(
-      () => this.setState({ isLoading: false }),
-      2500
-    );
-  }
-
+  componentDidMount() { this.timerHandle = setTimeout(() => this.setState({ isLoading: false }), 2500); }
   componentWillUnmount() {
     if (this.timerHandle) {
       clearTimeout(this.timerHandle);
@@ -38,23 +25,78 @@ class App extends React.Component {
   }
 
   render() {
-    if (this.state.isLoading) {
-      return (
-        // <Loading />
-        <div>its loading</div>
-      );
-    }
-
+    if (this.state.isLoading) { return ( <div>its loading</div> ); }
+  
     return (
       <div className="min-h-screen bg-yellow-25">
         <Navbar />
-        <ChooseBread order={this.state} />
-        {/* <ChooseMeat order={this.state} /> */}
-        {/* <ChooseVegetables order={this.state} /> */}
-        {/* <ChooseSauces order={this.state} /> */}
+        
+        {/* Choose Bread */}
+        <div id="ChooseBread" className="flex flex-col items-center justify-center my-5">
+          <div className="my-3 text-3xl font-bold text-center text-red-1000">
+            Choisissez le pain
+          </div>
+
+          <div className="flex flex-col items-center justify-center mt-5 space-x-4 sm:flex-row">
+            {defaultBreads.map((bread) => (
+              <div key={bread.name}>
+                <IngredientBox name={bread.name} image={bread.image} />
+              </div>
+            ))}
+          </div>
+          <Button />
+        </div>
+        
+        {/* Choose Meat */}
+          <div id="ChooseMeat" className="flex flex-col items-center justify-center my-5">
+            <div className="my-3 text-3xl font-bold text-center text-red-1000">
+              Passons à la viande... ou au tofu ?
+            </div>
+
+            <div className="flex flex-col items-center justify-center mt-5 space-x-4 sm:flex-row">
+              {defaultMeats.map((meat) => (
+                <div key={meat.name}>
+                  <IngredientBox name={meat.name} image={meat.image} />
+                </div>
+              ))}
+            </div>
+            <Button />
+          </div>
+
+        {/* Choose Vegetables */}
+        <div id="ChooseVegetables" className="flex flex-col items-center justify-center my-5">
+            <div className="my-3 text-3xl font-bold text-center text-red-1000">
+              Des crudités pour votre merveille ?
+            </div>
+
+            <div className="flex flex-col items-center justify-center mt-5 space-x-4 sm:flex-row">
+              {defaultVegetables.map((veg) => (
+                <div key={veg.name}>
+                  <IngredientBox name={veg.name} image={veg.image} />
+                </div>
+              ))}
+            </div>
+            <Button />
+          </div>
+
+        {/* Choose Sauces */}
+        <div id="ChooseSauces" className="flex flex-col items-center justify-center my-5">
+            <div className="my-3 text-3xl font-bold text-center text-red-1000">
+              Des sauces pour donner de la couleur à cette magie ?
+            </div>
+
+            <div className="flex flex-col items-center justify-center mt-5 space-x-4 sm:flex-row">
+              {defaultSauces.map((veg) => (
+                <div key={veg.name}>
+                  <IngredientBox name={veg.name} image={veg.image} />
+                </div>
+              ))}
+            </div>
+            <Button />
+        </div>
+
       </div>
     );
-  }
-}
+}};
 
 export default App;
