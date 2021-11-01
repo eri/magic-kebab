1bd06205ae7607f4484dba1a53f74b53bdf76777
Eray C <hi@eri.gg>
2021-11-01 15:46:57 +0100

    fix: Typo on tailwind config for colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,8 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      extend: {
+    extend: {
+      colors: {
         yellow: {
           25: "#FFF6D6",
         },
