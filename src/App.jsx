2e19128324781f454de19c8688e783e817effd35
Eray C <hi@eri.gg>
2021-11-01 13:25:19 +0100

    ref: Initialize App with a loading page

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import * as React from "react";
+import "./index.css";
+
+// Components
+import { Loading } from "./components/Loading";
+import { Navbar } from "./components/Navbar";
+
+// Views
+import { ChooseBread } from "./views/ChooseBread";
+import { ChooseMeat } from "./views/ChooseMeat";
+import { ChooseVegetables } from "./views/ChooseVegetables";
+import { ChooseSauces } from "./views/ChooseSauces";
+
+import { defaultOrder } from "./data";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentOrder: defaultOrder,
+      isLoading: true,
+      timerHandle: null,
+    };
+  }
+
+  componentDidMount() {
+    this.timerHandle = setTimeout(
+      () => this.setState({ isLoading: false }),
+      2500
+    );
+  }
+
+  componentWillUnmount() {
+    if (this.timerHandle) {
+      clearTimeout(this.timerHandle);
+      this.timerHandle = null;
+    }
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return (
+        // <Loading />
+        <div>its loading</div>
+      );
+    }
+
+    return (
+      <div className="min-h-screen bg-yellow-25">
+        {/* <Navbar /> */}
+        {/* <ChooseBread order={this.state} /> */}
+        <ChooseMeat order={this.state} />
+        {/* <ChooseVegetables order={this.state} /> */}
+        {/* <ChooseSauces order={this.state} /> */}
+      </div>
+    );
+  }
+}
+
+export default App;
